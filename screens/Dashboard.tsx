d8a040802299181cd25d71c5139697fc77da8cb1
Alan Rodriguez <alan.rodriguez@digitalfemsa.com>
2023-08-31 13:42:43 -0700

    Dashboard card finalizada

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -2,15 +2,18 @@ import { FlatList, StyleSheet, View } from "react-native";
 import React from "react";
 import CustomCard from "../components/CustomCard";
 import { Dish } from "../utils/types/Dish";
+import DashboardHeader from "../components/DashboardHeader";
 
 const Dashboard = () => {
   const mockedItem: Dish[] = [
     {
+      id: 1,
       title: "Articulo 1",
       description: "descripcion",
       price: 100,
     },
     {
+      id: 2,
       title: "Articulo 1",
       description: "descripcion",
       price: 100,
@@ -21,6 +24,8 @@ const Dashboard = () => {
       <FlatList
         renderItem={({ item }) => <CustomCard dish={item} />}
         data={mockedItem}
+        numColumns={2}
+        ListHeaderComponent={() => <DashboardHeader />}
       />
     </View>
   );
